eb795fdaeec2c246d4902c51708e4db8fa1c4246
Anandreddy-1010 <kanandr7@gmail.com>
2025-11-28 18:18:32 +0530

    Add files via upload

---
+++ NameToNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class NameToNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        try {
+            
+            String firstThree = name.substring(0, 3);
+            int num = Integer.parseInt(firstThree);
+            System.out.println("Converted number: " + num);
+        } 
+        catch (StringIndexOutOfBoundsException e) {
+            System.out.println("Error: Name must contain at least 3 characters!");
+        }
+        catch (NumberFormatException e) {
+            System.out.println("Error: First three characters are not numbers!");
+        }
+    }
+}
